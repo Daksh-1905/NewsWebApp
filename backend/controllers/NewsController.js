3fbd6ce86c34ab6691ae3bdcee10a4fe09a25b0f
Daksh-1905 <dakshjain1905@gmail.com>
2025-08-24 23:09:52 +0530

    api key added to .env

--- backend/controllers/NewsController.js
+++ backend/controllers/NewsController.js
@@ -1,7 +1,7 @@
 const showNews = async (req, res) => {
   try {
     let response = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=us&${req._parsedUrl.query}&apiKey=fff828e628364a319237d0d54d127e80`
+      `https://newsapi.org/v2/top-headlines?country=us&${req._parsedUrl.query}&apiKey=${process.env.NEWS_API_KEY}`
     );
     if (!response.ok) res.status(404).json({ message: "Error fetching news" });
     let data = await response.json();
